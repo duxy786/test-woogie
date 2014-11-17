93a949ab42bb6d8261895f46cc5473691dc1a82f
duxy786 <duxy786@gmail.com>
2014-11-17 08:43:50 +0000

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+class ApplicationController < ActionController::Base
+  def index
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def faq
+  end
+
+  def pricing
+  end
+
+  def features
+  end
+end
